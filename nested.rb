7f7da3cfa3abc3fadbef13c0aa1e902641eb24ad
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-15 17:56:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,11 +81,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-		 programmer_hash[:yukihiro_matsumoto] => {
+		 programmer_hash[:yukihiro_matsumoto] = {
 		    :known_for => "Ruby",
 		     :languages => ["LISP", "C"]
 		  }
-		
+
 
 end
 
