be9e81651a46ea35554c32c1caee485d6dbae40b
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-15 17:42:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -6,14 +6,14 @@ def hopper
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
+    #    :alan_kay => {
+    #      :known_for => "Object Orientation",
+    #      :languages => ["Smalltalk", "LISP"]
+    #    },
+    #    :dennis_ritchie => {
+    #      :known_for => "Unix",
+    #      :languages => ["C"]
+    #    }
      }
 
 
